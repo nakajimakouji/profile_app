6741c6fba06640809e04205b82bee3c5969849e6
nakajimakouji <totoro42ff@gmail.com>
2023-10-10 23:58:51 +0900

    add null:false to password_digest

--- db/migrate/20231008123509_create_users.rb
+++ db/migrate/20231008123509_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :email, null: false
-      t.string :password_digest
+      t.string :password_digest, null: false
       t.text :profile_text
       t.string :avatar_image
 
