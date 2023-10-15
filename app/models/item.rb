e2e33106900573c01d96b761a1aa67b4c9b7d6f3
nakajimakouji <totoro42ff@gmail.com>
2023-10-15 11:30:47 +0900

    validation each models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  # 数字、整数のみ、(greater_than)0以上
+  validates :study_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
