149120cbc1fc944c9b174a4955d865fa80a64607
nakajimakouji <totoro42ff@gmail.com>
2023-10-15 21:34:09 +0900

    ルートページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "static_pages#home"
+  get "static_pages/home"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
