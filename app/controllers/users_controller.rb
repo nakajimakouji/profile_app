91fb8271010f19f71f67e68ce4be122185221a38
nakajimakouji <totoro42ff@gmail.com>
2023-10-22 12:02:20 +0900

    SessionsHelperの共通化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+
   def new
     @user = User.new
   end
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "登録に成功しました！"
       redirect_to @user
     else
