20fdda086db8dbaeeebdb37d3b895cdaa6abf8de
nakajimakouji <totoro42ff@gmail.com>
2023-10-22 10:44:04 +0900

    登録の成功と失敗

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }, allow_nil: true
-  validates :profile_text, length: { minimum: 50, maximum: 199 }
+  validates :profile_text, length: { minimum: 50, maximum: 199 }, allow_nil: true
 end
