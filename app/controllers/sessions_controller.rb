5f114a92d3f482e241af91009f0a39703f43c6a4
nakajimakouji <totoro42ff@gmail.com>
2023-10-23 22:14:18 +0900

    フラッシュメッセージ編集

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       log_in user
       redirect_to root_path
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'ログインに失敗しました'
       render 'static_pages/home', status: :unprocessable_entity
     end
   end
