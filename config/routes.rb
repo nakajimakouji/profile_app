69ce639a734a8341f63b1a65bdecd5d48b1984ac
nakajimakouji <totoro42ff@gmail.com>
2023-11-06 22:38:36 +0900

    only制約の追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   post   "/login",   to: "sessions#create"
   delete "/logout",  to: "sessions#destroy"
   resources :users, only: %i[new create edit update destroy]
-  resources :items
+  resources :items, only: %i[index]
   resources :categories do
     resources :items, only: %i[new create edit update destroy]
   end
