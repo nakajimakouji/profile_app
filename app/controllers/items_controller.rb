3ad0f403e704e03ff78ba2a511e9a21530a80cba
nakajimakouji <totoro42ff@gmail.com>
2023-11-12 18:39:19 +0900

    学習項目の追加機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,4 +10,24 @@ class ItemsController < ApplicationController
     @item = Item.new
     @month = params[:month]
   end
+
+  def create
+    @category = Category.find_by(id: params[:category_id])
+    @month = params[:month]
+    @item = current_user.items.build(item_params.merge(category: @category))
+    if @item.save
+      flash[:success] = "項目追加に成功しました"
+      redirect_to items_url(month: @item.month)
+    else
+      flash.now[:danger] = "項目追加に失敗しました"
+      @month = params[:item][:month] 
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def item_params
+      params.require(:item).permit(:name, :study_time, :month)
+    end
 end
