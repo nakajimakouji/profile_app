17ad903d73bc2b5fca2ef51af2d2f964a2ece66e
nakajimakouji <totoro42ff@gmail.com>
2023-11-12 18:51:28 +0900

    itemモデルにnameのバリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
+  validates :name, presence: true
   # 数字、整数のみ、(greater_than)0以上
   validates :study_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
