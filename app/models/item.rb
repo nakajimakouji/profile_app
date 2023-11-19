84524db13317499014c11713c496fe04030a159d
nakajimakouji <totoro42ff@gmail.com>
2023-11-19 10:34:34 +0900

    エラーメッセージの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,6 @@ class Item < ApplicationRecord
   validates :name, presence: true
   # 数字、整数のみ、(greater_than)0以上
   validates :study_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # 何を軸にするか（今回はname）
+  validates :name, uniqueness: { scope: [:category_id, :month, :user_id], message: "が被っています" }
 end
