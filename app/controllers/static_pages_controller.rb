511919c46192f58e645aeef948c935e4fa59e46c
nakajimakouji <totoro42ff@gmail.com>
2023-12-06 23:30:31 +0900

    DRYの修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,19 +1,24 @@
 class StaticPagesController < ApplicationController
+  include SessionsHelper
+
   def home
-    @back_end = {
-      "先々月" => current_user.items.where(category: 1, month: Time.now.prev_month(2).month).sum(&:study_time),
-      "先月" => current_user.items.where(category: 1, month: Time.now.prev_month.month).sum(&:study_time),
-      "今月" => current_user.items.where(category: 1, month: Time.now.month).sum(&:study_time)
-    }
-    @front_end = {
-      "先々月" => current_user.items.where(category: 2, month: Time.now.prev_month(2).month).sum(&:study_time),
-      "先月" => current_user.items.where(category: 2, month: Time.now.prev_month.month).sum(&:study_time),
-      "今月" => current_user.items.where(category: 2, month: Time.now.month).sum(&:study_time)
-    }
-    @infra = {
-      "先々月" => current_user.items.where(category: 3, month: Time.now.prev_month(2).month).sum(&:study_time),
-      "先月" => current_user.items.where(category: 3, month: Time.now.prev_month.month).sum(&:study_time),
-      "今月" => current_user.items.where(category: 3, month: Time.now.month).sum(&:study_time)
-    }
+    if logged_in?
+      @back_end = generate_category_data(1)
+      @front_end = generate_category_data(2)
+      @infra = generate_category_data(3)
+    end
   end
+
+  private
+
+    def generate_category_data(category) {
+      "先々月" => study_time_for_category(category, Time.now.prev_month(2).month),
+      "先月" => study_time_for_category(category, Time.now.prev_month.month),
+      "今月" => study_time_for_category(category, Time.now.month)
+    }
+    end
+
+    def study_time_for_category(category, month)
+      current_user.items.where(category: category, month: month).sum(&:study_time)
+    end
 end
