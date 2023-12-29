166d152b9d68759efbc99fabd77d90064cc8680d
nakajimakouji <f.f-super1142@au.com>
2023-12-29 21:02:53 +0900

    マイグレーションファイルの修正

--- db/migrate/20231103080139_modify_item_table.rb
+++ db/migrate/20231103080139_modify_item_table.rb
@@ -1,7 +1,7 @@
 class ModifyItemTable < ActiveRecord::Migration[7.0]
   def change
     add_column :items, :month, :integer, null: false
+    add_index :items, [:user_id, :category_id, :name, :month], unique: true
+    remove_index :items, [:user_id, :category_id, :name], unique: true
   end
-  add_index :items, [:user_id, :category_id, :name, :month], unique: true
-  remove_index :items, [:user_id, :category_id, :name], unique: true
 end
